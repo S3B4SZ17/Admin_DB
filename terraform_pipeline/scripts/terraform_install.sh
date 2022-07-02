b22032add7f2bab388197ea27ff9c02a773e36ad
Sebastian Zumbado H <sebastian.zumbado@ibm.com>
2022-07-02 14:09:17 -0600

    :pencil: Fix: Changed the path for the install terraform script

--- terraform_pipeline/scripts/terraform_install.sh
+++ terraform_pipeline/scripts/terraform_install.sh
@@ -2,13 +2,19 @@
 # Installing terraform
 
 binaries::installTerraform(){
-    echo "Installing terraform version"
-    terraform_version=terraform_1.2.4
-    wget -q https://releases.hashicorp.com/terraform/${terraform_version}/terraform_${terraform_version}_linux_amd64.zip
-    unzip terraform_${terraform_version}_linux_amd64.zip
+    terraform version > /dev/null 2>&1
+    if [ $? -ne 0 ]; then
+        echo "Installing terraform version"
+        terraform_version=1.2.4
+        curl https://releases.hashicorp.com/terraform/${terraform_version}/terraform_${terraform_version}_linux_amd64.zip --output terraform_${terraform_version}_linux_amd64.zip
+        unzip terraform_${terraform_version}_linux_amd64.zip
 
-    chmod +x terraform
-    sudo cp terraform /usr/bin/terraform
-    terraform version
-    echo $PATH
+        chmod +x terraform
+        cp terraform /usr/local/bin/terraform
+        terraform version
+        echo $PATH
+    else
+        echo "Terraform already installed"
+    fi
+
 }
