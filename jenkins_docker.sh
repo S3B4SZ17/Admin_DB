c6f02bcfc9217a1613afa25c14404e0145b32d53
Sebastian Zumbado H <sebastian.zumbado@ibm.com>
2022-07-03 16:31:26 -0600

    :pencil: Feat: Updating the install scripts

--- jenkins_docker.sh
+++ jenkins_docker.sh
@@ -2,25 +2,28 @@
 ######################################
 # Script that will install a Jenkins server in a docker container
 ######################################
+install_Jenkins(){
+  docker ps > /dev/null 2>&1
+  if [[ $? -ne 0 ]]; then
+    echo "Docker is not running. Please start the daemon"
+    exit 1
+  fi
 
-docker ps > /dev/null 2>&1
-if [[ $? -ne 0 ]]; then
-  echo "Docker is not running. Please start the daemon"
-  exit 1
-fi
 
+  echo "Creating jenkins network"
+  docker network create jenkins
 
-echo "Creating jenkins network"
-docker network create jenkins
+  echo "Create jenkins-data volume"
+  docker volume create jenkins-data
 
-echo "Create jenkins-data volume"
-docker volume create jenkins-data
+  echo "Running the jenkins-docker container"
+  docker run -it --network="jenkins" --network-alias="myjenkins" \
+    --name jenkins-docker --detach \
+    --volume jenkins-data:/var/jenkins_home \
+    --restart=on-failure \
+    --publish 8080:8080 \
+    --publish 50000:50000 \
+    jenkins/jenkins:jdk11
+}
 
-echo "Running the jenkins-docker container"
-docker run -it --network="jenkins" --network-alias="myjenkins" \
-  --name jenkins-docker --detach \
-  --volume jenkins-data:/var/jenkins_home \
-  --restart=on-failure \
-  --publish 8080:8080 \
-  --publish 50000:50000 \
-  jenkins/jenkins:jdk11
+install_Jenkins
--- vault_install.sh
+++ vault_install.sh
@@ -1,5 +1,29 @@
 #!/bin/bash
 # Install a dev Vault container
 
-echo "Run a Dev Vault container"
-docker run --name vault-docker --cap-add=IPC_LOCK -e 'VAULT_DEV_ROOT_TOKEN_ID=myroot' -e VAULT_ADDR='http://0.0.0.0:8200' -p 8200:8200 --network="jenkins" vault &
+install_Vault(){
+    echo "Run a Dev Vault container"
+    docker run --name vault-docker --cap-add=IPC_LOCK -e 'VAULT_DEV_ROOT_TOKEN_ID=myroot' -e VAULT_ADDR='http://0.0.0.0:8200' -p 8200:8200 --network="jenkins" vault &
+}
+
+configure_Vualt(){
+    # In this case is we are hardcoding the token. JUST FOR DEV
+    vault login -no-print myroot
+    
+    # Enable Approle for authentication between machines (Jenkins and Vault)
+    vault auth enable approle
+    
+    # Create a new approle named jenkins that will last forever
+    vault write auth/approle/role/jenkins \
+    token_num_uses=0 \
+    secret_id_num_uses=0 \
+    policies="jenkins-policy"
+
+    # Writing the jenkins-policy
+    echo "path "secret/*" {
+        capabilities = ['read','create','update']
+    }" > jenkins-policy.hcl
+
+    # Applying the new policy
+    vault policy write jenkins-policy jenkins-policy.hcl
+}
