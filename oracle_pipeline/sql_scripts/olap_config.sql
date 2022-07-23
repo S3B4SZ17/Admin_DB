b4ceb2cd9fb03fd3dabb6bda8bed9c75d65a680f
S3B4SZ17 <sebaszh17@gmail.com>
2022-07-22 19:56:39 -0600

    :pencil: Feat: Add tasks to validate if the oracle_DB container exists

--- oracle_pipeline/sql_scripts/olap_config.sql
+++ oracle_pipeline/sql_scripts/olap_config.sql
@@ -1,6 +1,6 @@
 --- Creation of tablespaces
-CREATE TABLESPACE glob DATAFILE '$HOME/proyecto_final/tablespaces/dato_glob.dbf' SIZE 3M AUTOEXTEND ON NEXT 12K MAXSIZE 10M;
-CREATE TEMPORARY TABLESPACE globtmp TEMPFILE '$HOME/proyecto_final/tablespaces/temp_glob.dbf' SIZE 10M AUTOEXTEND ON NEXT 64K MAXSIZE 20M;
+CREATE TABLESPACE glob DATAFILE '/home/oracle/proyecto_final/tablespaces/dato_glob.dbf' SIZE 3M AUTOEXTEND ON NEXT 12K MAXSIZE 10M;
+CREATE TEMPORARY TABLESPACE globtmp TEMPFILE '/home/oracle/proyecto_final/tablespaces/temp_glob.dbf' SIZE 10M AUTOEXTEND ON NEXT 64K MAXSIZE 20M;
 
 --- User GLOBAL creation
 CREATE USER "GLOBAL" IDENTIFIED BY password
