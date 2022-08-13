e263f64387c641253d6f6f1d9d2888e45e0d78c6
S3B4SZ17 <sebaszh17@gmail.com>
2022-08-13 11:39:41 -0600

    :pencil: Feat: Remove the Analytical Workspace manager tool

--- oracle_pipeline/Analytic_Workspace_Manager_12.2/awm.bat
+++
@@ -1,9 +0,0 @@
-setlocal
-rem    Modify the path to your installed Java Run-time Environment
-rem    Make sure your environment has at least JRE version 1.8 installed
-set path=c:\Program Files\java\jre8\bin;%path%
-
-rem    Start AwM.  -mx sets the size of the memory allocated to the heap by the Java Virtual machine
-start javaw -mx1024m -jar awm12.2.0.1.0.jar
-
-endlocal
